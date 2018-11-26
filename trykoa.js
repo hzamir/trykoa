4498573c1ac8cb26019fa9f172107feeeb06cbaf
Haim Zamir <hzamir@gmail.com>
2018-11-25 21:57:19 -0500

    add in static server, and router api prefix

--- trykoa.js
+++ trykoa.js
@@ -1,10 +1,10 @@
 const Koa     = require('koa');
+const serve   = require('koa-static');
 const Router  = require('koa-router');
 const compose = require('koa-compose');
 const mw      = require('./somemiddlewares');
 
 
-const router = new Router();
 const app    = new Koa();
 
 
@@ -23,6 +23,8 @@ function failing () { throw new Error('bad result')}
 
 const cannedResponse = ()=>({a:1, b:2, c:'hi'});
 
+const router = new Router();
+router.prefix('/api');
 router.get('/hello/:id',  dobody(()=>'hello'));
 router.get('/goodbye',    dobody(()=>'goodbye'));
 router.get('/failing',    dobody(failing));
@@ -32,6 +34,7 @@ router.get('/', (ctx)=>{ ctx.status = 404;});
 
 app.use(router.routes());
 app.use(router.allowedMethods());
+app.use(serve('.'));
 
 //... notes as it stands, unsupported routes are marked with ok instead of not found
 
